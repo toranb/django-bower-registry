3331ddc41446c1b40cdd6cc7ff3a85af60e7bd5b
Bradley Weston <bradwestonwigston@gmail.com>
2014-11-06 15:43:37 +0000

    Allow dots in package names.
    Packages such as `backbone.wreqr` and `backbone.babysitter` where 404ing fixes #9

--- registry/urls.py
+++ registry/urls.py
@@ -5,5 +5,5 @@ from api.views import PackagesListView, PackagesFindView, PackagesSearchView
 urlpatterns = patterns('',
     url(r'^packages/?$', PackagesListView.as_view(), name='list'),
     url(r'^packages/(?P<name>[-\w]+)/?$', PackagesFindView.as_view(), name='find'),
-    url(r'^packages/search/(?P<name>[-\w]+)/?$', PackagesSearchView.as_view(), name='search')
+    url(r'^packages/search/(?P<name>[-\w\.]+)/?$', PackagesSearchView.as_view(), name='search')
 )
