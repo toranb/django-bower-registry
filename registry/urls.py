70d3240d97a78060a85a695230207ef733b9ffeb
Bradley Weston <bradwestonwigston@gmail.com>
2014-11-06 20:07:27 +0000

    Opps, forgot the normal package view too.

--- registry/urls.py
+++ registry/urls.py
@@ -4,6 +4,6 @@ from api.views import PackagesListView, PackagesFindView, PackagesSearchView
 
 urlpatterns = patterns('',
     url(r'^packages/?$', PackagesListView.as_view(), name='list'),
-    url(r'^packages/(?P<name>[-\w]+)/?$', PackagesFindView.as_view(), name='find'),
+    url(r'^packages/(?P<name>[-\w\.]+)/?$', PackagesFindView.as_view(), name='find'),
     url(r'^packages/search/(?P<name>[-\w\.]+)/?$', PackagesSearchView.as_view(), name='search')
 )
